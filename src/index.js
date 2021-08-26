84ec535381b26c38781c40fafe1836dc74976df9
unknown <hoang01663011503@yahoo.com>
2021-08-26 15:12:27 +0700

    render

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
+const route = require('./routes/index.js');
+let Parser = require('rss-parser');
+let parser = new Parser();
+
 
 // add morgan lib for dev project
 app.use(morgan('combined'));
@@ -20,12 +24,13 @@ app.set('views', path.join(__dirname, '/resources/views'));
 
 // Use static file
 app.use(express.static(path.join(__dirname + '/public')));
-// console.log('PATH', path.join(__dirname + '/public'));
 
-// routingggg
-app.get('/', function (req, res) {
-    res.render('home');
-});
+// Add to use req.body dataView
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// route
+route(app);
 
 app.listen(port, () => {
     console.log(`News app listening at http://localhost:${port}`);
